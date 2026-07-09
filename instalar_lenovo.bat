3a7f55869593401519bc4e0d19c04088055495c5
Rafael Olivares <sar_colombia_valle@hotmail.com>
2026-07-09 10:15:25 -0500

    ui(inventario): always show both annulment buttons to avoid confusion

--- instalar_lenovo.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -ExecutionPolicy Bypass -Command "& ([scriptblock]::Create((irm 'https://github.com/Rafaelolivares07/TUC-TUC/releases/download/agentes/instalar_sar.ps1'))) -Cliente lenovo -Nombre 'Lenovo Pruebas'"
-pause

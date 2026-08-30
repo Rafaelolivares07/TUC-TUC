942722ab19bd722cb601fdb0db9d41321d981362
Rafael Olivares <sar_colombia_valle@hotmail.com>
2026-08-30 14:55:23 -0500

    fix: vincular Kardex query - remove product filter when numero_doc is set

--- app/blueprints/inventarios.py
+++ app/blueprints/inventarios.py
@@ -1727,7 +1727,7 @@ def api_vincular_ids_contabilidad(negocio_id):
         if fecha_hasta:
             k_where += " AND COALESCE(m.documento_fecha, m.created_at::date) <= %s"
             k_params.append(fecha_hasta)
-        if prod_padre_id:
+        if prod_padre_id and not numero_doc:
             k_where += " AND m.producto_padre_id = %s"
             k_params.append(prod_padre_id)
 
